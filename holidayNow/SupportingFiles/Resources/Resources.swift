c26a99cd5eb0f3d6cfbc06a8ea7faee0f9b8f170
Georgii <neguritsageorgiy@gmail.com>
2023-09-01 18:32:12 +0400

    add statics

--- holidayNow/SupportingFiles/Resources/Resources.swift
+++ holidayNow/SupportingFiles/Resources/Resources.swift
@@ -1,14 +1,11 @@
-//
-//  Resources.swift
-//  holidayNow
-//
-//  Created by Евгений on 01.09.2023.
-//
-
 import UIKit
 
 enum Resources {
     enum Images {
         static let onboardingImage = UIImage(named: "OnboardingImage")
+    }
+    
+    enum API {
+        static let deepAI = "13fc0b0b-50a8-4acd-8ced-0ad045c64746"
     }
 }
