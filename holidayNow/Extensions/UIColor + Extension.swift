d29aae64222fc67546cbf681658ca468f52496d4
esplusnin <esplusnin94@yandex.ru>
2023-09-01 12:52:52 +0400

    + SwiftGen, ресурсы, экстеншн под шрифты и цвета

---
+++ holidayNow/Extensions/UIColor + Extension.swift
@@ -0,0 +1,16 @@
+//
+//  UIColor + Extension.swift
+//  holidayNow
+//
+//  Created by Евгений on 01.09.2023.
+//
+
+import UIKit
+
+extension UIColor {
+    static let blackDay = UIColor(named: "Black[day]")
+    static let lightGray = UIColor(named: "LightGray")
+    static let lightUniversalRed = UIColor(named: "LightUniversalRed")
+    static let universalRed = UIColor(named: "UniversalRed")
+    static let whiteDay = UIColor(named: "White[day]")
+}
---
+++ holidayNow/Extensions/UIFont + Extension.swift
@@ -0,0 +1,23 @@
+//
+//  UIFont + Extension.swift
+//  holidayNow
+//
+//  Created by Евгений on 01.09.2023.
+//
+
+import UIKit
+
+extension UIFont {
+    static let headerLargeBoldFont = UIFont.boldSystemFont(ofSize: 40)
+    static let headerMediumBoldFont = UIFont.boldSystemFont(ofSize: 24)
+    static let headerSmallBoldFont = UIFont.boldSystemFont(ofSize: 20)
+    
+    static let captionLargeBoldFont = UIFont.boldSystemFont(ofSize: 18)
+    static let captionMediumBoldFont = UIFont.boldSystemFont(ofSize: 16)
+    static let captionSmallBoldFont = UIFont.boldSystemFont(ofSize: 14)
+    
+    static let bodyLargeRegularFont = UIFont.systemFont(ofSize: 20)
+    static let bodyMediumRegularFont = UIFont.systemFont(ofSize: 18)
+    static let bodySmallRegularFont = UIFont.systemFont(ofSize: 14)
+    static let bodyExtraSmallRegularFont = UIFont.systemFont(ofSize: 12)
+}
