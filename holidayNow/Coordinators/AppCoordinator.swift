213c9d580baf68d9624263abb408f1666d71012c
Georgii <neguritsageorgiy@gmail.com>
2023-09-05 12:51:30 +0400

    add marks

--- holidayNow/Coordinators/AppCoordinator.swift
+++ holidayNow/Coordinators/AppCoordinator.swift
@@ -1,14 +1,18 @@
 import UIKit
 
 final class AppCoordinator: CoordinatorProtocol {
-    var navigationController: UINavigationController
-    var viewControllerFactory: ViewControllerFactoryProtocol
     
+    // MARK: - Constants and Variables:
+    internal var navigationController: UINavigationController
+    internal var viewControllerFactory: ViewControllerFactoryProtocol
+    
+    // MARK: - LifeCycle:
     init(navigationController: UINavigationController, viewControllerFactory: ViewControllerFactoryProtocol) {
         self.navigationController = navigationController
         self.viewControllerFactory = viewControllerFactory
     }
     
+    // MARK: - Public Methods
     func start() {
         let onboardingViewController = viewControllerFactory.createOnboardingViewController()
         navigationController.pushViewController(onboardingViewController, animated: true)
--- holidayNow/Coordinators/CoordinatorProtocol.swift
+++ holidayNow/Coordinators/CoordinatorProtocol.swift
@@ -2,6 +2,7 @@ import UIKit
 
 protocol CoordinatorProtocol {
     var navigationController: UINavigationController { get set }
+    var viewControllerFactory: ViewControllerFactoryProtocol { get set }
     
     func start()
     func goToSuccessResultViewController()
