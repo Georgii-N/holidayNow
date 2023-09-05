a13c50f76e16b669227a36e4fb083288316054f6
Georgii <neguritsageorgiy@gmail.com>
2023-09-05 19:32:08 +0400

    add viewmodel logic

--- holidayNow/Scenes/ResponseScreens/SuccessViewModel.swift
+++ holidayNow/Scenes/ResponseScreens/SuccessViewModel.swift
@@ -2,7 +2,16 @@ import Foundation
 
 final class SuccessViewModel {
     
-    @Observable
-    private(set) var  responseText: String?
+    // MARK: - Observable Values
+    var textResultObservable:
+    Observable<String?> {
+        $textResult
+    }
     
+    @Observable
+    private var textResult: String?
+    
+    init(textResult: String) {
+        self.textResult = textResult
+    }
 }
