d33ba407485624389f910358970acb639f17c89e
Georgii <neguritsageorgiy@gmail.com>
2023-09-13 19:17:18 +0400

    add MArks

--- holidayNow/Services/AmplitudeService/AnalyticsService.swift
+++ holidayNow/Services/AmplitudeService/AnalyticsService.swift
@@ -2,14 +2,17 @@ import AmplitudeSwift
 
 final class AnalyticsService {
     
+    // MARK: - Constants and Variables:
     static let instance = AnalyticsService()
-    
-    private init () {}
     
     private let amplitude = Amplitude(configuration: Configuration(
            apiKey: Resources.API.amplitude
        ))
     
+    // MARK: - Lifecycle:
+    private init () {}
+    
+    // MARK: - Public Methods:
     func trackAmplitudeEvent(with name: String, params: [String: Any]?) {
         amplitude.track(eventType: name, eventProperties: params)
     }
