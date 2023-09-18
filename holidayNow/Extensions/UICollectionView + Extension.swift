fbf53cbe64ed8ed4de12fee5fb8de70ba93a8fdb
Georgii <neguritsageorgiy@gmail.com>
2023-09-18 14:06:18 +0400

    add urlrequest timeout, delete assertion failure

--- holidayNow/Extensions/UICollectionView + Extension.swift
+++ holidayNow/Extensions/UICollectionView + Extension.swift
@@ -3,7 +3,7 @@ import UIKit
 extension UICollectionView {
     func dequeueReusableCell<T: UICollectionViewCell>(indexPath: IndexPath, with identificator: String) -> T {
         guard let cell = dequeueReusableCell(withReuseIdentifier: identificator, for: indexPath) as? T else {
-            assertionFailure("Could not dequeue cell with identifier: \(identificator) for: \(indexPath)")
+            print("Could not dequeue cell with identifier: \(identificator) for: \(indexPath)")
             return T()
         }
         return cell
--- holidayNow/Scenes/ResponseScreens/WaitingScreen/WaitingViewModel.swift
+++ holidayNow/Scenes/ResponseScreens/WaitingScreen/WaitingViewModel.swift
@@ -39,7 +39,7 @@ final class WaitingViewModel: WaitingViewModelProtocol {
                 
             case .failure(let error):
                 self.isResponseSuccess = false
-                assertionFailure(error.localizedDescription)
+                print(error.localizedDescription)
             }
         }
     }
