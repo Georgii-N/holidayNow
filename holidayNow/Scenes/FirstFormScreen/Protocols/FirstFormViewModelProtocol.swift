520373820d085cc683051aa2c9c6c0a0329b5007
esplusnin <esplusnin94@yandex.ru>
2023-11-16 13:20:20 +0400

    #64 Поправил баг с некорректных множественным выделением ячеек на первом экране формы

--- holidayNow/Scenes/FirstFormScreen/Protocols/FirstFormViewModelProtocol.swift
+++ holidayNow/Scenes/FirstFormScreen/Protocols/FirstFormViewModelProtocol.swift
@@ -3,8 +3,8 @@ import Foundation
 protocol FirstFormViewModelProtocol: AnyObject {
     var indexToRemoveCell: Int? { get }
     var userNameObservable: Observable<String?> { get }
-    var ownCellCounterObservable: Observable<Int> { get }
     var interestsObservable: Observable<Interest> { get }
+    var selectedInterestsObservable: Observable<[GreetingTarget]> { get }
     func setupUsername(_ name: String?)
     func controlInterestState(isAdd: Bool, interest: GreetingTarget)
     func addNewOwnInterest(name: String)
